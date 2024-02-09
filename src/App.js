f17366b4cf7364e4971a30c7d06cd7c734e73676
SinisterSan <138826506+SinisterSan@users.noreply.github.com>
2024-02-09 12:38:24 +0700

    Removed "Environment" from drei removed the sunset environment from drei, as it is no longer needed.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
-import { Canvas } from '@react-three/fiber'
-import { OrbitControls, Stars, Stats, Environment } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Stars, Stats } from "@react-three/drei";
 import { PCFSoftShadowMap, sRGBEncoding } from "three";
 import { useState, Suspense } from "react";
-import { useControls } from 'leva'
+import { useControls } from "leva";
 
 import useHexGridPoints from "./hooks/useHexGridPoints";
 import HexGrid from "./components/HexGrid";
 import Lights from "./components/Lights";
-import { Overlay } from './components/Overlay';
+import { Overlay } from "./components/Overlay";
 
 export default function App() {
-
   const gridSize = 16;
   const spacing = 1.02;
   const points = useHexGridPoints(gridSize, spacing);
@@ -22,50 +21,66 @@ export default function App() {
 
   const controls = useControls({
     AutoRotate: true,
-    Sunset: true
+    // Sunset: true,
   });
-
 
   const seasons = ["Summer", "Winter"];
   const [selectedSeason, setSelectedSeason] = useState("Summer");
-  
+
   useControls({
     Themes: {
       options: [...seasons],
       onChange: (value) => {
         setSelectedSeason(value);
       },
-    }
+    },
   });
 
-  const Sunset = () => {
-    return (controls.Sunset) && <Environment preset="sunset" background blur={0.4} />
-  }
-
+  // const Sunset = () => {
+  //   return (
+  //     controls.Sunset && <Environment preset="sunset" background blur={0.4} />
+  //   );
+  // };
 
   return (
     <>
-      <Canvas dpr={[1, 2]} shadows gl={{ antialias: true, physicallyCorrectLights: true, toneMappingExposure: 0.5, shadowMap: { enabled: true, type: PCFSoftShadowMap }, outputEncoding: sRGBEncoding }} camera={{ fov: 75, position: [38, 38, 40]}} orthographic={false} >
-
+      <Canvas
+        dpr={[1, 2]}
+        shadows
+        gl={{
+          antialias: true,
+          physicallyCorrectLights: true,
+          toneMappingExposure: 0.5,
+          shadowMap: { enabled: true, type: PCFSoftShadowMap },
+          outputEncoding: sRGBEncoding,
+        }}
+        camera={{ fov: 75, position: [38, 38, 40] }}
+        orthographic={false}
+      >
         <Suspense fallback={null}>
-          <group position={[0, 0, 0]} key={generate+selectedSeason} >
+          <group position={[0, 0, 0]} key={generate + selectedSeason}>
             <HexGrid points={points} season={selectedSeason} />
           </group>
         </Suspense>
 
-        <OrbitControls autoRotate={controls.AutoRotate} autoRotateSpeed={0.4} enablePan={false}/>
+        <OrbitControls
+          autoRotate={controls.AutoRotate}
+          autoRotateSpeed={0.4}
+          enablePan={false}
+        />
         <Stars radius={50} depth={50} count={400} factor={2} />
         <Stats />
         <Lights />
-        <Sunset />
-
+        {/* <Sunset /> */}
       </Canvas>
 
-      <div className='btnContainer'>
-        <button onClick={setGenerateValue} className="genBtn" >Regenerate</button>
+      <div className="btnContainer">
+        <button onClick={setGenerateValue} className="genBtn">
+          Regenerate
+        </button>
       </div>
 
       <Overlay />
     </>
-  )
+  );
 }
